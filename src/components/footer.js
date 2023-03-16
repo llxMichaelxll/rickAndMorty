acec466ebb917f6bf1a2f8440f446c0ad241e33a
llxMichaelxll <palacioshmichaela@gmail.com>
2023-03-16 12:38:32 -0500

    menu-footer

---
+++ src/components/footer.js
@@ -0,0 +1,20 @@
+import "./footer.css";
+
+
+export function Foteer() {
+    return <div className="contenedor-footer">
+
+        <div className="b-footer">
+            <p>Copyright © Todos los derechos reservados</p>
+        </div>
+
+        
+            <ul className="c-footer">
+                <li className="botones-footer"><a src="#"><img></img></a></li>
+                <li className="botones-footer"><a src="#"><img></img></a></li>
+                <li className="botones-footer"><a src="#"><img></img></a></li>
+            </ul>
+        
+
+    </div>
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {Menu} from "./components/menu"
+import {Foteer} from "./components/footer"
+import {Saludo,Summa} from "./Ejemplo1";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+
+root.render(
+<>
+    <Menu/>
+    <Foteer/>
+</>)
